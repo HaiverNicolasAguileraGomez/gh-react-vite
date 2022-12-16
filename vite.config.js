db2901ed10f8895a02ad1caef9c423c5ea5e2003
Haiver Nicolás Aguilera Gómez <haguilera@valemas.com>
2022-12-16 12:54:49 -0500

    clase mamona

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://haivernicolasaguileragomez.github.io/gh-react-vite/gh-react-vite/',
+  base: '/',
   plugins: [react()],
 })
